e137e4793d1603f985645a072c5ec1b228a742b1
rob <rob@awakeheart.net>
2017-08-10 22:58:23 +0200

    unrealized P/L models

--- BittrexTradeAnalyzer/Controllers/HomeController.cs
+++ BittrexTradeAnalyzer/Controllers/HomeController.cs
@@ -21,15 +21,22 @@ namespace BittrexTradeAnalyzer.Controllers
     [HttpPost]
     public ActionResult Index(TradeInput inp)
     {
-    
+
+      TradeData tradedata = CalculateTradeData(inp);
+      //ViewBag.Result = outp;
+      ViewBag.TradeData = tradedata;
+      return View(inp);
+    }
+
+    private static TradeData CalculateTradeData(TradeInput inp)
+    {
       var bittrex = new Exchange();
-     
       var coin = inp.Coin.ToUpper();
       var basecurr = inp.BaseCurrency;
       bittrex.Initialise(new ExchangeContext { ApiKey = inp.ApiKey, Secret = inp.ApiSecret, QuoteCurrency = basecurr.ToUpper(), Simulate = false });
-
+      var tick = bittrex.GetTicker(coin);
       var hist = bittrex.GetOrderHistory(coin, inp.NoOfTrades).OrderBy(x => x.TimeStamp);
-      string outp="";
+      string outp = "";
 
       //    var buys = hist.Where(x=>x.OrderType == OpenOrderType.Limit_Buy);
       //    var sells = hist.Where(x=>x.OrderType == OpenOrderType.Limit_Sell);
@@ -38,6 +45,10 @@ namespace BittrexTradeAnalyzer.Controllers
       decimal averageBuy = 0;
       decimal buyQuantity = 0;
       bool first = true;
+      var tradedata = new TradeData()
+      {
+        RealizedTrades = new List<RealizedTradeData>()
+      };
       foreach (var h in hist)
       {
         if (h.OrderType == OpenOrderType.Limit_Buy)
@@ -45,28 +56,62 @@ namespace BittrexTradeAnalyzer.Controllers
           decimal prevBuyQ = buyQuantity;
           buyQuantity += h.Quantity - h.QuantityRemaining;
           averageBuy = (averageBuy * prevBuyQ) / buyQuantity + (h.PricePerUnit * ((h.Quantity - h.QuantityRemaining) / buyQuantity));
-          outp += $"Bought {h.Quantity - h.QuantityRemaining} {coin} at {h.PricePerUnit} - averagebuy: {averageBuy:0.#########} date:{h.TimeStamp}<br/>";
+          //outp += $"Bought {h.Quantity - h.QuantityRemaining} {coin} at {h.PricePerUnit} - averagebuy: {averageBuy:0.#########} Value:{h.Price} date:{h.TimeStamp}<br/>";
+          tradedata.RealizedTrades.Add(new RealizedTradeData
+          {
+            Quantity = h.Quantity - h.QuantityRemaining,
+            AverageBuy = averageBuy,
+            PricePerUnit = h.PricePerUnit,
+            TotalPrice = h.Price,
+            TimeStamp = h.TimeStamp,
+            BuySell = "Buy"
+          });
           first = false;
         }
         else
         {
           var sold = h.Quantity - h.QuantityRemaining;
           if (first)
-            outp += $"First order is sell, ignoring!!! Sold {sold} at {h.PricePerUnit} date: {h.TimeStamp}<br/>";
+          {
+            //outp += $"First order is sell, ignoring!!! Sold {sold} at {h.PricePerUnit} date: {h.TimeStamp}<br/>";
+          }
           else
           {
             var pl = ((h.PricePerUnit - averageBuy) / averageBuy) * 100;
             buyQuantity -= sold;
-            outp += $"Sold {sold} at {h.PricePerUnit} P/L:{pl:0.##}% remaining:{buyQuantity} date: {h.TimeStamp}<br/>";
+            // outp += $"Sold {sold} at {h.PricePerUnit} P/L:{pl:0.##}% remaining:{buyQuantity} Value: {h.Price} date: {h.TimeStamp}<br/>";
+            tradedata.RealizedTrades.Add(new RealizedTradeData
+            {
+              Quantity = sold,
+              RemainingQuantity = buyQuantity,
+              AverageBuy = averageBuy,
+              ProfitLoss = pl,
+              PricePerUnit = h.PricePerUnit,
+              TotalPrice = h.Price,
+              TimeStamp = h.TimeStamp,
+              BuySell = "Sell"
+            });
           }
         }
 
       }
-    
-      ViewBag.Result = outp;
-      return View(inp);
-    }
+      if (buyQuantity > 0)
+      {
+        decimal unrealized = ((tick.Last - averageBuy) / averageBuy) * 100;
+        outp += $"<h3>Unrealized P/L: last:{tick.Last} {unrealized:0.##} Quantity: {buyQuantity} Value: {buyQuantity * tick.Last}</h3>";
+        tradedata.UnrealizedTrade = new UnrealizedTrade
+        {
+          ProfitLoss = unrealized,
+          ProfitLossValue = (tick.Last * buyQuantity) - (averageBuy * buyQuantity),
+          RemainingQuantity = buyQuantity,
+          Value = buyQuantity * tick.Last,
+          LastPrice = tick.Last
+        };
 
+      }
+
+      return tradedata;
+    }
 
     public ActionResult About()
     {
---
+++ BittrexTradeAnalyzer/Models/TradeData.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Web;
+
+namespace BittrexTradeAnalyzer.Models
+{
+
+  public class RealizedTradeData
+  {
+    public string BuySell { get; set; }
+    [DisplayFormat(DataFormatString = "{0:#.########}", ApplyFormatInEditMode = true)]
+    public decimal Quantity { get; set; }
+    [DisplayFormat(DataFormatString = "{0:#.########}", ApplyFormatInEditMode = true)]
+    public decimal RemainingQuantity { get; set; }
+    [DisplayFormat(DataFormatString = "{0:#.########}", ApplyFormatInEditMode = true)]
+    public decimal PricePerUnit { get; set; }
+    [DisplayFormat(DataFormatString = "{0:#.########}", ApplyFormatInEditMode = true)]
+    public decimal AverageBuy { get; set; }
+    [DisplayFormat(DataFormatString = "{0:#.########}", ApplyFormatInEditMode = true)]
+    public decimal TotalPrice { get; set; }
+    public decimal ProfitLoss { get; set; }
+    public DateTime TimeStamp { get; set; }
+  }
+
+  public class UnrealizedTrade
+  {
+    [DisplayFormat(DataFormatString = "{0:#.########}", ApplyFormatInEditMode = true)]
+    public decimal RemainingQuantity { get; set; }
+    [DisplayFormat(DataFormatString = "{0:#.########}", ApplyFormatInEditMode = true)]
+    public decimal Value { get; set; }
+    [DisplayFormat(DataFormatString = "{0:#.##}", ApplyFormatInEditMode = true)]
+    public decimal ProfitLoss { get; set; }
+    [DisplayFormat(DataFormatString = "{0:#.########}", ApplyFormatInEditMode = true)]
+    public decimal ProfitLossValue { get; set; }
+    [DisplayFormat(DataFormatString = "{0:#.########}", ApplyFormatInEditMode = true)]
+    public decimal LastPrice { get; set; }
+
+  }
+
+  public class TradeData
+  {
+    public IList<RealizedTradeData> RealizedTrades { get; set; }
+    public UnrealizedTrade UnrealizedTrade { get; set; }
+  }
+
+
+}
